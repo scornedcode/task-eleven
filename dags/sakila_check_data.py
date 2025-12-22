e6826876f719ed79621253e00c79a6533a47b061
scorned <scornedcode@gmail.com>
2025-12-22 16:06:33 +0100

    add Task flow API and sf_hook instead SF Operator

--- dags/sakila_check_data.py
+++ dags/sakila_check_data.py
@@ -1,32 +1,34 @@
 from airflow import DAG
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.decorators import task
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from airflow.utils.dates import days_ago
 from datetime import timedelta
 
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
 with DAG(
-    dag_id='sakila_check_data',
-    default_args=default_args,
-    description='check data of Sakila in Snowflake',
-    schedule_interval=None,
-    start_date=days_ago(1),
-    catchup=False,
-    tags=['check', 'snowflake', 'sakila'],
+        dag_id='sakila_check_data',
+        default_args=default_args,
+        schedule_interval=None,
+        start_date=days_ago(1),
+        catchup=False,
+        tags=['check', 'snowflake', 'sakila'],
 ) as dag:
+    @task(task_id='check_table_rows')
+    def check_rows(table_name: str, conn_id: str = 'snowflake_conn') -> int:
+        hook = SnowflakeHook(snowflake_conn_id=conn_id)
 
-    check_sakila = SnowflakeOperator(
-        task_id='sakila_check_rows',
-        snowflake_conn_id='snowflake_conn',
-        sql='SELECT count(*) FROM RAW_DATA.SAKILA.ACTOR HAVING count(*) > 0;',
-        warehouse='AIRBYTE_WH',
-        database='RAW_DATA',
-        role='DEV_ROLE'
-    )
+        current_count = hook.get_first(f"SELECT count(*) FROM {table_name}")[0]
+
+        print(f"Table {table_name} has {current_count} rows.")
+
+        if current_count == 0:
+            raise ValueError(f"Check failed: Table {table_name} is empty!")
+
+        return current_count
+
+    check_rows(table_name="RAW_DATA.SAKILA.ACTOR")
